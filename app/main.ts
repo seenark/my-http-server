d79ecb60c9bc5bb5b07aae230753ec256a20fed1
Atiwat Seenark <um_oom@hotmail.com>
2024-05-04 15:16:39 +0700

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -7,10 +7,24 @@ const server = net.createServer((socket) => {
     const rcvdData = data.toString();
     console.log("rcvd", rcvdData);
     const [firstline] = rcvdData.split("\r\n");
-    const [method, path, httpVersion] = firstline.split(" ");
+    const [tethod, path, httpVersion] = firstline.split(" ");
     if (path === "/") {
       console.log("path", path);
       socket.write("HTTP/1.1 200 OK\r\n\r\n");
+      return;
+    }
+    if (path.includes("/echo/")) {
+      const pathWithoutEcho = path.replace("/echo/", "");
+      console.log("pathWithoutEcho", pathWithoutEcho);
+      const responseArr = [
+        "HTTP/1.1 200 OK",
+        "Content-Type: text/plain",
+        `Content-Length: ${pathWithoutEcho.length}`,
+        `\n${pathWithoutEcho}`,
+      ];
+      const responseStr = responseArr.join("\r\n");
+      console.log("responseStr", responseStr);
+      socket.write(`${responseStr}\r\n\r\n`);
       return;
     }
     socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
