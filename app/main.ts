59898940a7fa3c86a2c8c2c632e86830839dd3ce
Atiwat Seenark <um_oom@hotmail.com>
2024-05-04 09:29:19 +0700

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,14 +1,24 @@
 import * as net from "node:net";
 
+console.clear();
+
 const server = net.createServer((socket) => {
-  socket.on("data", () => {
-    socket.write("HTTP/1.1 200 OK\r\n\r\n");
-    socket.end();
+  socket.on("data", (data) => {
+    const rcvdData = data.toString();
+    console.log("rcvd", rcvdData);
+    const [firstline] = rcvdData.split("\r\n");
+    const [method, path, httpVersion] = firstline.split(" ");
+    if (path === "/") {
+      console.log("path", path);
+      socket.write("HTTP/1.1 200 OK\r\n\r\n");
+      return;
+    }
+    socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
   });
 });
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
+console.log("Logs from your program will appear here!!");
 
 // Uncomment this to pass the first stage
 server.listen(4221, "localhost", () => {
